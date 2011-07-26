52bf6e17251df5d643b054e3cac9fffdc6fa62a4
Sergey Kozhakov <phewgin@gmail.com>
2011-07-26 13:42:21 +0400

    billing:работоспособный api

--- apps/api/registry.py
+++ apps/api/registry.py
@@ -25,6 +25,9 @@ from examination.models import CardTemplate, ExaminationCard
 from helpdesk.models import Issue, IssueType
 from scheduler.models import Calendar, Event
 from django.contrib import messages
+from billing.models import Account, Payment, ClientAccount
+from interlayer.models import ClientItem
+from django.contrib.contenttypes.models import ContentType
 #from reporting.models import Report, FieldItem, GroupItem, SummaryItem, Fields,\
 #    Groups, Summaries, FilterItem, Filters
 
@@ -985,8 +988,80 @@ class IssueResource(ExtResource):
             'status':ALL,
             'operator':ALL_WITH_RELATIONS
         }        
+        
+class ClientItemResource(ExtResource):
+
+    def dehydrate(self, bundle):
+        bundle.data['client_name'] = bundle.obj.client.name
+        return bundle
+    
+    class Meta:
+        queryset = ClientItem.objects.all()
+        resource_name = 'clientitem'
+        authorization = DjangoAuthorization()
+        filtering = {
+            'client' : ALL_WITH_RELATIONS,
+        }
 
 
+class ContentTypeResource(ExtResource):
+    class Meta:
+        queryset = ContentType.objects.all()
+        resource_name = 'contenttype'
+        authorization = DjangoAuthorization()
+        allowed_methods = ['get']
+
+class AccountResource(ExtResource):
+    
+    class Meta:
+        queryset = Account.objects.all()
+        resource_name = 'account'
+        authorization = DjangoAuthorization()
+        
+class ClientAccountResource(ExtResource):
+    client_item = fields.ForeignKey(ClientItemResource, 'client_item')
+    account = fields.ForeignKey(AccountResource, 'account')
+    
+    def dehydrate(self, bundle):
+        bundle.data['client_name'] = bundle.obj.client_item.client.name
+        bundle.data['account_id'] = bundle.obj.account.id
+        bundle.data['amount'] = bundle.obj.account.amount
+        return bundle
+    
+    class Meta:
+        queryset = ClientAccount.objects.all().select_related()
+        resource_name = 'clientaccount'
+        authorization = DjangoAuthorization()
+        filtering = {
+            'client_item' : ALL_WITH_RELATIONS,
+        }
+        
+class PaymentResource(ExtResource):
+    client_account = fields.ForeignKey(ClientAccountResource, 'client_account')
+    content_type = fields.ForeignKey(ContentTypeResource, 'content_type', null=True)
+    
+    def dehydrate(self, bundle):
+        bundle.data['client_name'] = bundle.obj.client_account.client_item.client.name
+        bundle.data['client'] = bundle.obj.client_account.client_item.client
+        bundle.data['account_id'] = bundle.obj.client_account.account.id
+        bundle.data['amount'] = abs(bundle.obj.amount)
+        return bundle
+    
+    def obj_create(self, bundle, request=None, **kwargs):
+        kwargs['operator']=request.user
+        result = super(PaymentResource, self).obj_create(bundle=bundle, request=request, **kwargs)
+        return result
+    
+    class Meta:
+        queryset = Payment.objects.all()
+        resource_name = 'payment'
+        authorization = DjangoAuthorization()
+        limit = 1000
+        filtering = {
+            'id' : ALL,
+            'doc_date' : ALL,
+        }
+        
 
 api = Api(api_name=get_api_name('dashboard'))
 
@@ -1052,6 +1127,15 @@ api.register(IssueResource())
 #scheduler
 api.register(CalendarResource())
 api.register(EventResource())
+
+#interlayer
+api.register(ClientItemResource())
+
+#billing
+api.register(AccountResource())
+api.register(ContentTypeResource())
+api.register(ClientAccountResource())
+api.register(PaymentResource())
 
 #reporting
 #api.register(ReportResource())
