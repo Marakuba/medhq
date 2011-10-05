a0fbc91577ea9dbd16725d7ce6e19a0f43810d9d
Anatoly <4015555@gmail.com>
2011-10-05 23:23:42 +0400

    DBTemplates cache backend

--- settings/common.py
+++ settings/common.py
@@ -43,6 +43,8 @@ SECRET_KEY = 'ud)-iik%3k(6%p_tzbt-e-kvgvekxi4-y+!xjnhpl0_e)_3gat'
 
 CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
 
+DBTEMPLATES_CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
+
 #JOHNNY_MIDDLEWARE_KEY_PREFIX='jc_medical'
 
 TEMPLATE_LOADERS = (
