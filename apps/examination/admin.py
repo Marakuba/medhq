475502d19a5b84d81780ac9f2c7699a10013719e
Anatoly <4015555@gmail.com>
2011-10-29 09:23:47 +0400

    Добавлено поле "Лаборант" в карту осмотра

--- apps/examination/admin.py
+++ apps/examination/admin.py
@@ -16,7 +16,24 @@ class ExaminationCardAdmin(admin.ModelAdmin):
     """
     """
     exclude = ('ordered_service',)
+    list_display = ('created','service','patient','assistant')
+    list_editable = ('assistant',)
+    search_fields = ('ordered_service__order__patient',)
+    date_hierarchy = 'created'
     inlines = [DICOMAdmin]
+    
+    def service(self, obj):
+        return obj.ordered_service.service
+    service.short_description = u'Услуга'
+    
+    def patient(self, obj):
+        return obj.ordered_service.order.patient.short_name()
+    patient.short_description = u'Пациент'
+    
+    def icd(self, obj):
+        return obj.mbk_diag or u'<em>не указано</em>'
+    icd.short_description = u'Диагноз по МКБ'
+    icd.allow_tags = True
     
 class CardTemplateAdmin(admin.ModelAdmin):
     """
