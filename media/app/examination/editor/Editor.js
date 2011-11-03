a45ef2b46a25c4dbb74a569e0c9c079be3a59b33
Sergey Kozhakov <phewgin@gmail.com>
2011-11-03 15:23:07 +0400

    some commit

--- media/app/examination/editor/Editor.js
+++ media/app/examination/editor/Editor.js
@@ -6,59 +6,51 @@ App.examination.Editor = Ext.extend(Ext.Panel, {
 	initComponent : function() {
 
 		this.serviceTree = new App.ServicePanel.Tree({
-			layout: 'fit'
-			,region:'west'
-			,width:250 
-			,border: false
-			,collapsible:true
-			,collapseMode:'mini'
-			,split:true
+			layout: 'fit',
+			region:'west',
+			width:250,
+			border: false,
+			collapsible:true,
+			collapseMode:'mini',
+			split:true
 		})
 				
 		this.templateBody = new App.examination.TemplateBody ({
-			id:'grid'
-			,anchor: '0 -250'
-			,border: false	
-			,enableDragDrop:true
-			,ddGroup:'grid2tree' //same as for tree
+			id:'grid',
+			anchor: '0 -250',
+			border: false,	
+			enableDragDrop:true,
+			ddGroup:'grid2tree' //same as for tree
 		});
 		
-		this.glossaryTree = new App.examination.GlossaryTree ({
-			height: 225	
-			,border: false
-		});
+//		this.glossaryTree = new App.examination.GlossaryTree ({
+//			height: 225,	
+//			border: false
+//		});
 		
 				
 		var config = {
-			id: 'ServiceAdminPanel_id'
-			,title: 'Конструктор'
-			,layout: 'border'	
-     		,items: [
+			id: 'editor-cmp',
+			title: 'Конструктор',
+			layout: 'border',	
+     		items: [
 				this.serviceTree,
 				{
-				xtype: 'panel'
-				,id: 'servicecenterpanel'
-				,region:'center'
-     			,border:false
-     			,layout: 'anchor'				
-	    		,items: [
-	    			this.ServiceGrid
-	    			,{
-	    				xtype: 'panel'
-	    				,id: 'tabpanelcontainer'
-	    				,layout: 'fit'
-	    				,border: false
-	    				,title: 'Услуги'
-	    				,items: this.InfoTabPanel 
-	    			}
+				xtype: 'panel',
+				id: 'servicecenterpanel',
+				region:'center',
+     			border:false,
+     			layout: 'anchor',				
+	    		items: [
+	    			this.templateBody
 	    		]}
 			]
 		}
 								
 		Ext.apply(this, Ext.apply(this.initialConfig, config));
-		App.service.ServiceAdminPanel.superclass.initComponent.apply(this, arguments);
+		App.examination.Editor.superclass.initComponent.apply(this, arguments);
 	}
 });
 
 
-Ext.reg('serviceadminpanel', App.service.ServiceAdminPanel);
+Ext.reg('editor', App.examination.Editor);
