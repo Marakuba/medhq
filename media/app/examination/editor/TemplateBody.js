5ffba0e9b50e1e2d12a70e9af8b153cfb0297549
Sergey Kozhakov <phewgin@gmail.com>
2011-11-15 17:48:26 +0400

    EXCARD:создана модель названий разделов; названия управляются в админке

--- media/app/examination/editor/TemplateBody.js
+++ media/app/examination/editor/TemplateBody.js
@@ -5,45 +5,12 @@ App.examination.TemplateBody = Ext.extend(Ext.TabPanel, {
 	
 	initComponent: function(){
 		
-		this.anamBtn = {
-			text:'Анамнез',
-			id:'anamBtn',
-			sequence:0,
-			handler:this.onAddSection.createDelegate(this,['anamnesis','Анамнез','anamBtn',0]),
-			scope:this
-		}; 
-		this.diagBtn = {
-			text:'Диагноз',
-			sequence:1,
-			handler:this.onAddSection.createDelegate(this,['diagnosis','Диагноз','diagBtn',1]),
-			id:'diagBtn',
-			scope:this
-		};
-		this.conclBtn = {
-			text:'Заключение',
-			sequence:2,
-			handler:this.onAddSection.createDelegate(this,['conclusion','Заключение','conclBtn',2]),
-			id:'conclBtn',
-			scope:this
-		};
+		this.menuBtns = {};
 		
-		this.menu = new Ext.menu.Menu({
-			items:[this.anamBtn,this.diagBtn,this.conclBtn]
+		this.sectionMenu = new Ext.menu.Menu({
+			items:[]
 		});
 		
-		this.addSecBtn = new Ext.SplitButton({
-			text:'Добавить раздел',
-			menu:this.menu,
-			scope:this
-		});
-		
-		this.ttb = [this.addSecBtn,{
-				xtype:'button',
-				iconCls:'silk-add',
-				text:'Добавить подраздел',
-				handler:this.onAddSubsection.createDelegate(this, [])
-			}]
-			
 		
 		this.newTab = Ext.extend(Ext.Panel,{
 			title:'Новый раздел',
@@ -51,6 +18,42 @@ App.examination.TemplateBody = Ext.extend(Ext.TabPanel, {
 			cls: 'placeholder',
 			closable: true
 		});
+		
+		Ext.Ajax.request({
+			url:App.getApiUrl('examfieldset'),
+			success:function(response, opts){
+				var obj = Ext.decode(response.responseText);
+				Ext.each(obj.objects,function(rec){
+					this.menuBtns[rec.name] = {
+						text:rec.title,
+						id:rec.name,
+						order:rec.order,
+						handler:this.onAddSection.createDelegate(this,[rec.name,rec.title,rec.order]),
+						scope:this
+					};
+					this.sectionMenu.insert(rec.order,this.menuBtns[rec.name])
+				},this);
+			},
+			failure:function(response, opts){
+				this.addSecBtn.disable();
+			},
+			scope:this
+		});
+		
+		this.addSecBtn = new Ext.SplitButton({
+			text:'Добавить раздел',
+			menu:this.sectionMenu,
+			scope:this
+		});
+		
+		this.addSubSecBtn = new Ext.Button({
+			iconCls:'silk-add',
+			text:'Добавить подраздел',
+			disabled:true,
+			handler:this.onAddSubsection.createDelegate(this, [])
+		});
+		
+		this.ttb = [this.addSecBtn, this.addSubSecBtn]
 			
 		config = {
 			region:'center',
@@ -62,24 +65,28 @@ App.examination.TemplateBody = Ext.extend(Ext.TabPanel, {
 		App.examination.TemplateBody.superclass.initComponent.apply(this, arguments);
 	},
 	
-	onAddSection: function(name,title,btn,seq){
-		var new_tab = this.insert(seq,new this.newTab({
+	onAddSection: function(btn,title,order){
+		var new_tab = this.insert(order,new this.newTab({
 			title:title,
 			btn:btn,
-			seq:seq,
-			name:name,
+			order:order,
+			btn:btn,
 			listeners:{
-				'beforeclose': function(p){
-					this.menu.insert(p.seq,this[p.btn]);
+				'close': function(p){
+					this.sectionMenu.insert(p.order,this.menuBtns[p.btn]);
 					this.addSecBtn.enable();
+					if (this.items.length == 1) {
+						this.addSubSecBtn.disable();
+					}
 				},
 				scope:this
 			}
 		}));
-		this.menu.remove(btn);
-		if (this.menu.items.length == 0) {
+		this.sectionMenu.remove(btn);
+		if (this.sectionMenu.items.length == 0) {
 			this.addSecBtn.disable();
-		}
+		};
+		this.addSubSecBtn.enable();
 		this.setActiveTab(new_tab);
 		this.doLayout();
 	},
