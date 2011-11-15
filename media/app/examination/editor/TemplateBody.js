4c1efc7a0953a3d8896b2da02d554c706ee24870
Sergey Kozhakov <phewgin@gmail.com>
2011-11-15 15:37:19 +0400

    EXCARD:добавление/удаление пунктов меню Добавить раздел

--- media/app/examination/editor/TemplateBody.js
+++ media/app/examination/editor/TemplateBody.js
@@ -5,12 +5,39 @@ App.examination.TemplateBody = Ext.extend(Ext.TabPanel, {
 	
 	initComponent: function(){
 		
-		this.ttb = [{
-				xtype:'button',
-				iconCls:'silk-add',
-				text:'Добавить раздел',
-				handler:this.onAddSection.createDelegate(this, [])
-			},{
+		this.anamBtn = {
+			text:'Анамнез',
+			id:'anamBtn',
+			sequence:0,
+			handler:this.onAddSection.createDelegate(this,['anamnesis','Анамнез','anamBtn',0]),
+			scope:this
+		}; 
+		this.diagBtn = {
+			text:'Диагноз',
+			sequence:1,
+			handler:this.onAddSection.createDelegate(this,['diagnosis','Диагноз','diagBtn',1]),
+			id:'diagBtn',
+			scope:this
+		};
+		this.conclBtn = {
+			text:'Заключение',
+			sequence:2,
+			handler:this.onAddSection.createDelegate(this,['conclusion','Заключение','conclBtn',2]),
+			id:'conclBtn',
+			scope:this
+		};
+		
+		this.menu = new Ext.menu.Menu({
+			items:[this.anamBtn,this.diagBtn,this.conclBtn]
+		});
+		
+		this.addSecBtn = new Ext.SplitButton({
+			text:'Добавить раздел',
+			menu:this.menu,
+			scope:this
+		});
+		
+		this.ttb = [this.addSecBtn,{
 				xtype:'button',
 				iconCls:'silk-add',
 				text:'Добавить подраздел',
@@ -22,31 +49,37 @@ App.examination.TemplateBody = Ext.extend(Ext.TabPanel, {
 			title:'Новый раздел',
 			autoScroll:true,
 			cls: 'placeholder',
-//			layout:{
-//		        type:'fit',
-//		        align:'stretch'
-//			},
 			closable: true
 		});
 			
-		this.anamnesisBody = new Ext.Panel({
-			closable:true,
-			autoScroll:true,
-			cls: 'placeholder',
-			title:'Анамнез'
-		});
 		config = {
 			region:'center',
 			margins:'5 0 5 5',
-			items:[this.anamnesisBody],
+			items:[],
 			tbar:this.ttb
 		}
 		Ext.apply(this, Ext.apply(this.initialConfig, config));
 		App.examination.TemplateBody.superclass.initComponent.apply(this, arguments);
 	},
 	
-	onAddSection: function(){
-		var new_tab = this.add(new this.newTab);
+	onAddSection: function(name,title,btn,seq){
+		var new_tab = this.insert(seq,new this.newTab({
+			title:title,
+			btn:btn,
+			seq:seq,
+			name:name,
+			listeners:{
+				'beforeclose': function(p){
+					this.menu.insert(p.seq,this[p.btn]);
+					this.addSecBtn.enable();
+				},
+				scope:this
+			}
+		}));
+		this.menu.remove(btn);
+		if (this.menu.items.length == 0) {
+			this.addSecBtn.disable();
+		}
 		this.setActiveTab(new_tab);
 		this.doLayout();
 	},
