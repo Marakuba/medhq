39b946b9e14d5481220996424a9b6a0a2d004efd
Sergey Kozhakov <phewgin@gmail.com>
2011-11-21 13:48:31 +0400

    EXCARD:взаимосвязь дерева услуг и панели данных в шаблонах

--- media/app/examination/editor/Editor.js
+++ media/app/examination/editor/Editor.js
@@ -8,11 +8,11 @@ App.examination.Editor = Ext.extend(Ext.Panel, {
 		this.tmpStore = new Ext.data.RESTStore({
 			autoSave: true,
 			autoLoad : false,
-			apiUrl : get_api_url('extendedservice'),
+			apiUrl : get_api_url('examtemplate'),
 			model: App.models.Template
 		});
 		
-		this.serviceTree = new App.visit.VisitServicePanel({
+		this.serviceTree = new App.visit.VisitServicePanel ({
 			layout: 'fit',
 			region:'west',
 			width:250,
@@ -22,33 +22,15 @@ App.examination.Editor = Ext.extend(Ext.Panel, {
 			split:true
 		});
 		
-		this.tmpBody = new App.examination.TemplateBody ({
-			id:'tmp-body',
-			anchor: '0 -250',
-			border: false,	
-			enableDragDrop:true,
-			ddGroup:'grid2tree' //same as for tree
-		});
-		
-		this.startPanel = new App.examination.StartPanel({
-			id:'start'
-		});
 		
 		this.contentPanel = new Ext.Panel({
 			region:'center',
  			border:false,
  			layout: 'fit',				
     		items: [
-    			this.startPanel
     		]
 		});
 		
-//		this.glossaryTree = new App.examination.GlossaryTree ({
-//			height: 225,	
-//			border: false
-//		});
-		
-				
 		var config = {
 			id: 'editor-cmp',
 			closable:true,
@@ -60,16 +42,6 @@ App.examination.Editor = Ext.extend(Ext.Panel, {
 			]
 		};
 		
-		this.startPanel.on('opentmp',function(source){
-			if (!source){
-				this.record = new this.tmpStore.model();
-				this.contentPanel.remove(this.startPanel);
-				this.tmpBody.record = this.record;
-				this.contentPanel.add(this.tmpBody);
-				this.contentPanel.doLayout();
-			}
-		},this);
-		
 		Ext.apply(this, Ext.apply(this.initialConfig, config));
 		App.examination.Editor.superclass.initComponent.apply(this, arguments);
 		
@@ -79,8 +51,84 @@ App.examination.Editor = Ext.extend(Ext.Panel, {
 	onServiceClick: function(attrs){
 		var ids = attrs.id.split('-');
 		var id = ids[0];
-		this.service = '';
+		this.base_service = '';
 		
+		Ext.Ajax.request({
+			url:get_api_url('baseservice'),
+			method:'GET',
+			params: {id: id},
+			success:function(resp, opts) {
+				var jsonResponse = Ext.util.JSON.decode(resp.responseText);
+				Ext.each(jsonResponse.objects, function(item,i){
+					this.base_service = item.resource_uri;
+					console.log(this.base_service);
+				}, this);
+				
+				this.tmpStore.setBaseParam('base_service',App.uriToId(this.base_service));
+				this.tmpStore.load({
+					callback:function(records,opts,success){
+						if (records.length){
+							this.contentPanel.removeAll(true);
+							this.tmpBody = this.newTmpBody({
+								base_service : this.service,
+								record : records[0]
+							});
+							this.contentPanel.add(this.tmpBody);
+							this.contentPanel.doLayout();
+							
+						} else {
+							this.contentPanel.removeAll(true);
+							this.startPanel = this.newStartPanel();
+							this.contentPanel.add(this.startPanel);
+							this.contentPanel.doLayout();
+						}
+					},
+					scope:this
+				});
+			},
+			scope:this
+		});
+		
+		Ext.Ajax.request({
+			url:get_api_url('position'),
+			method:'GET',
+			params: {id: active_profile},
+			success:function(resp, opts) {
+				var jsonResponse = Ext.util.JSON.decode(resp.responseText);
+				Ext.each(jsonResponse.objects, function(item,i){
+					this.staff = item.staff;
+					console.log(this.staff);
+				}, this);
+			},
+			scope:this
+		});
+		
+	},
+	
+	newTmpBody: function(config){
+		return new App.examination.TemplateBody (config);
+	},
+	
+	newStartPanel: function(config){
+		var startPanel = new App.examination.StartPanel(config);
+		
+		startPanel.on('opentmp',function(source){
+			if (!source){
+				this.record = new this.tmpStore.model();
+				this.contentPanel.removeAll(true);
+				this.record.set('base_service',this.base_service);
+				this.record.set('staff',this.staff);
+				this.tmpStore.add(this.record);
+				this.tmpBody = this.newTmpBody({
+					record:this.record,
+					base_service:this.service
+				})
+				this.contentPanel.add(this.tmpBody);
+				this.contentPanel.doLayout();
+			}
+		},this);
+		
+		return startPanel;
 	}
 });
 
