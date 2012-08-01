8d494d159930f1bacf2fd674eed5085ffe3a6f83
Phewgin <phewgin@gmail.com>
2012-08-01 12:34:14 +0400

    сохранение направлений из медецинский стандартов

--- apps/medstandart/migrations/0001_initial.py
+++ apps/medstandart/migrations/0001_initial.py
@@ -84,7 +84,7 @@ class Migration(SchemaMigration):
         db.create_table('medstandart_standartitem', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('standart', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['medstandart.Standart'])),
-            ('base_service', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['service.BaseService'])),
+            ('service', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['service.ExtendedService'])),
             ('frequency', self.gf('django.db.models.fields.DecimalField')(default=1, max_digits=7, decimal_places=4)),
             ('average', self.gf('django.db.models.fields.DecimalField')(default=1, max_digits=7, decimal_places=4)),
         ))
@@ -122,6 +122,48 @@ class Migration(SchemaMigration):
         db.delete_table('medstandart_standartitem')
 
     models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'lab.tube': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Tube'},
+            'bc_count': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
         'medstandart.agecategory': {
             'Meta': {'ordering': "('name',)", 'object_name': 'AgeCategory'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -149,7 +191,7 @@ class Migration(SchemaMigration):
         },
         'medstandart.standart': {
             'Meta': {'ordering': "('name',)", 'object_name': 'Standart'},
-            'age_category': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['medstandart.AgeCategory']", 'null': 'True', 'blank': 'True'}),
+            'age_category': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'age_category'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['medstandart.AgeCategory']"}),
             'age_from': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
             'age_to': ('django.db.models.fields.PositiveIntegerField', [], {'default': '250'}),
             'complications': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['medstandart.Complications']", 'null': 'True', 'blank': 'True'}),
@@ -159,14 +201,14 @@ class Migration(SchemaMigration):
             'nosological_form': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['medstandart.NosologicalForm']", 'null': 'True', 'blank': 'True'}),
             'phase': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['medstandart.Phase']", 'null': 'True', 'blank': 'True'}),
             'stage': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['medstandart.Stage']", 'null': 'True', 'blank': 'True'}),
-            'terms': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['medstandart.Term']", 'null': 'True', 'blank': 'True'})
+            'terms': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'terms'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['medstandart.Term']"})
         },
         'medstandart.standartitem': {
             'Meta': {'ordering': "('standart',)", 'object_name': 'StandartItem'},
             'average': ('django.db.models.fields.DecimalField', [], {'default': '1', 'max_digits': '7', 'decimal_places': '4'}),
-            'base_service': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['service.BaseService']"}),
             'frequency': ('django.db.models.fields.DecimalField', [], {'default': '1', 'max_digits': '7', 'decimal_places': '4'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'service': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['service.ExtendedService']"}),
             'standart': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['medstandart.Standart']"})
         },
         'medstandart.term': {
@@ -184,7 +226,7 @@ class Migration(SchemaMigration):
             'prefix': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
             'reset_on': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'tag': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
-            'valid_till': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 7, 20, 0, 0)'})
+            'valid_till': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 8, 1, 0, 0)'})
         },
         'service.baseservice': {
             'Meta': {'ordering': "('name',)", 'object_name': 'BaseService'},
@@ -227,6 +269,18 @@ class Migration(SchemaMigration):
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
+        'service.extendedservice': {
+            'Meta': {'unique_together': "(('base_service', 'state'),)", 'object_name': 'ExtendedService'},
+            'base_service': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['service.BaseService']"}),
+            'branches': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'branches'", 'symmetrical': 'False', 'to': "orm['state.State']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_manual': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'staff': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['staff.Position']", 'null': 'True', 'blank': 'True'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['state.State']"}),
+            'tube': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'tube'", 'null': 'True', 'to': "orm['lab.Tube']"}),
+            'tube_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
         'service.icd10': {
             'Meta': {'ordering': "('code',)", 'object_name': 'ICD10'},
             'code': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
@@ -262,6 +316,73 @@ class Migration(SchemaMigration):
             'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['service.StandardService']"}),
             'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'staff.position': {
+            'Meta': {'ordering': "('staff__last_name', 'staff__first_name')", 'object_name': 'Position'},
+            'department': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['state.Department']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'staff': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['staff.Staff']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'staff.staff': {
+            'Meta': {'ordering': "('last_name', 'first_name')", 'object_name': 'Staff'},
+            'birth_day': ('django.db.models.fields.DateField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'guardian': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'high_school': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'high_school_end_date': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'home_address_street': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'home_phone': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id_card_issue_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id_card_number': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'id_card_org': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            'id_card_series': ('django.db.models.fields.CharField', [], {'max_length': '6', 'null': 'True', 'blank': 'True'}),
+            'id_card_type': ('django.db.models.fields.CharField', [], {'max_length': '1', 'null': 'True', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'medical_experience': ('django.db.models.fields.CharField', [], {'max_length': '2', 'blank': 'True'}),
+            'mid_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'mobile_phone': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
+            'operator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'operator_in_staff'", 'to': "orm['auth.User']"}),
+            'spec_experience': ('django.db.models.fields.CharField', [], {'max_length': '2', 'blank': 'True'}),
+            'speciality': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'\\xd0\\xb4'", 'max_length': '1'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'blank': 'True', 'related_name': "'staff_user'", 'unique': 'True', 'null': 'True', 'to': "orm['auth.User']"}),
+            'work_address_street': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'work_phone': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'})
+        },
+        'state.department': {
+            'Meta': {'object_name': 'Department'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['state.State']"})
+        },
+        'state.state': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'State'},
+            'address_street': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'bank_details': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'fax': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'head': ('django.db.models.fields.CharField', [], {'max_length': '150', 'null': 'True', 'blank': 'True'}),
+            'head_title': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inn': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '12', 'decimal_places': '0', 'blank': 'True'}),
+            'kpp': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '9', 'decimal_places': '0', 'blank': 'True'}),
+            'licenses': ('django.db.models.fields.CharField', [], {'max_length': '150', 'null': 'True', 'blank': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'official_title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'ogrn': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '15', 'decimal_places': '0', 'blank': 'True'}),
+            'phones': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'post_address': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'print_name': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '200'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'uuid': ('django.db.models.fields.CharField', [], {'max_length': '36', 'blank': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
         }
     }
 
--- media/app/examination/medstandarts/MedStandartServiceGrid.js
+++ media/app/examination/medstandarts/MedStandartServiceGrid.js
@@ -1,186 +1,65 @@
-Ext.ns('App.examorder');
+Ext.ns('App.examination', 'App.models');
 
-App.examorder.ExamOrderGrid = Ext.extend(Ext.grid.GridPanel, {
+App.examination.MedStandartServiceGrid = Ext.extend(Ext.grid.GridPanel, {
 
 	initComponent : function() {
 		
-		this.staff = App.getApiUrl('staff',active_staff);
+		this.store = new Ext.data.RESTStore({
+			autoLoad : false,
+			apiUrl : get_api_url('standartitem'),
+			model: App.models.StandartItemModel,
+			disabled:true
+		});
 		
-		this.tmp_id = Ext.id();
+		this.choiceBtn = new Ext.Button({
+			text:'Выбрать',
+			handler:this.onServiceChoice.createDelegate(this,[]),
+			iconCls:'silk-accept'
+		})
 		
-		this.backend = App.getBackend('examservice');		
+		this.sm = new Ext.grid.CheckboxSelectionModel({
+			listeners: {
+				rowselect:function(model,ind,rec) {
+					this.btnCheckStatus()
+				},
+				rowdeselect: function() {
+					this.btnCheckStatus()
+				},
+				scope:this
+			}
+		});
 		
-		this.store = this.backend.store;
-		
-		this.columns =  [{
-		    	header: "№ заказа", 
+		this.columns =  [this.sm,{
+		    	header: "Услуга", 
 		    	width: 60, 
 		    	sortable: true, 
-		    	dataIndex: 'barcode'
+		    	dataIndex: 'base_service_name'
 		    },{
-		    	header: "Дата", 
-		    	width: 60, 
-		    	sortable: true, 
-		    	dataIndex: 'created',
-		    	renderer:Ext.util.Format.dateRenderer('d.m.Y')
+		    	header: "Частота предоставления", 
+		    	width: 20,
+		    	dataIndex: 'frequency'
 		    },{
-		    	header: "Пациент", 
-		    	width: 300,
-		    	dataIndex: 'patient_full'
-		    },{
-		    	header: "Исследование", 
-		    	width: 600, 
-		    	sortable: true, 
-		    	dataIndex: 'service_name'
-
-		    },{
-		    	header: "Выполнено", 
-		    	width: 120, 
-		    	sortable: true, 
-		    	dataIndex: 'executed',
-		    	renderer:function(val, meta, record) {
-		    		var p = record.data.executed;
-		    		var flag = p ? 'yes' : 'no';
-		    		var img = "<img src='"+MEDIA_URL+"admin/img/admin/icon-"+flag+".gif'>"
-		    		return String.format("{0} {1}", img, p ? Ext.util.Format.date(p, 'd.m.Y H:i') : "");
-		    	} 
+		    	header: "Среднее количество", 
+		    	width: 20,
+		    	dataIndex: 'average'
 		    }];		
 		
-		this.historyBtn = new Ext.Button({
-			text:'История пациента',
-			iconCls:'silk-package',
-			disabled:true,
-			handler:this.onOpenHistory,
-			scope:this
-		});
-		this.addBtn = new Ext.Button({
-			text:'Добавить карту осмотра',
-			iconCls:'silk-add',
-			disabled:true,
-			handler:this.onAdd.createDelegate(this, [])
-		});
-		
-		this.ttb = new Ext.Toolbar({ 
-			items:[this.addBtn,{
-				text:'Подтвердить выполнение',
-				iconCls:'silk-accept',
-				id: this.tmp_id + 'order-exec',
-				disabled:true,
-				handler:function(){
-					var record = this.getSelected()
-					if (record && !record.data.executed) {
-						var now = new Date();
-						Ext.Msg.confirm('Заказ выполнен','Подтвердить выполнение?',function(btn){
-            				if (btn=='yes') {
-            					record.beginEdit();
-								record.set('executed', now);
-								record.endEdit();		
-            				}
-			        	},this);
-					}
-				},
-				scope:this
-			},'-',this.historyBtn/*,'-',{
-				xtype:'splitbutton',
-				text:'Открыть',
-				handler:this.onOpen.createDelegate(this, ['order']),
-				menu:{
-					items:[{
-						id:'all-by-visit',
-						text:'Открыть карты осмотра',
-						handler:this.onOpen.createDelegate(this,['order'])
-					},{
-						id:'all-by-lab',
-						text:'Открыть все карты осмотра',
-						handler:this.onOpen.createDelegate(this,['patient'])
-					}]
-				},
-				scope:this
-			}*/,'->','Период',{
-				xtype:'datefield',
-				format:'d.m.Y',
-				emptyText:'с',
-				listeners: {
-					select: function(df, date){
-						this.storeFilter('created__gte',date.format('Y-m-d 00:00'));
-					},
-					scope:this
-				}
-			},{
-				xtype:'datefield',
-				format:'d.m.Y',
-				emptyText:'по',
-				listeners: {
-					select: function(df, date){
-						this.storeFilter('created__lte',date.format('Y-m-d 23:59'));
-					},
-					scope:this
-				}
-			},'-',{
-                text: 'Текущие',
-                iconCls:'silk-hourglass',
-                enableToggle: true,
-                pressed: false,
-                toggleHandler: function(btn, pressed) {
-                	if (pressed) {
-                		this.store.filter('')
-                    	this.store.setBaseParam('executed__isnull',true)
-                    	this.store.load()
-                	} else {
-                		delete this.store.baseParams['executed__isnull']
-                		this.store.load()
-                	}
-                },
-                scope: this
-            },'-',{
-				text:'Обновить',
-				iconCls:'x-tbar-loading',
-				scope:this,
-				handler:function(){
-					this.store.load()
-				}
-			}]
-		}); 
-		
 		var config = {
-			id:'examorder-grid',
-			title:'Журнал заказов',
 			loadMask : {
 				msg : 'Подождите, идет загрузка...'
 			},
 			border : false,
 			store:this.store,
 			columns:this.columns,
-			sm : new Ext.grid.RowSelectionModel({
-				singleSelect : true,
-				listeners: {
-					rowselect:function(model,ind,rec) {
-						if (!rec.data.executed) {
-							Ext.getCmp(this.tmp_id+'order-exec').enable()
-							this.addBtn.enable();
-						} else {
-							this.addBtn.disable();
-						};
-						this.historyBtn.enable();
-						
-					},
-					rowdeselect: function() {
-						Ext.getCmp(this.tmp_id+'order-exec').disable();
-						this.addBtn.disable();
-						this.historyBtn.disable()
-					},
-					scope:this
-				}
-			}),
-	        tbar:this.ttb,
+			sm : this.sm,
 			listeners: {
-				rowdblclick:this.onAdd.createDelegate(this)
 			},
 			viewConfig:{
 				forceFit:true
 			},
+			tbar:[this.choiceBtn],
 			bbar: new Ext.PagingToolbar({
-	            pageSize: 25,
+	            pageSize: 100,
 	            store: this.store,
 	            displayInfo: true,
 	            displayMsg: 'Показана запись {0} - {1} из {2}',
@@ -189,115 +68,34 @@ App.examorder.ExamOrderGrid = Ext.extend(Ext.grid.GridPanel, {
 		};
 		
 		Ext.apply(this, Ext.apply(this.initialConfig, config));
-		App.examorder.ExamOrderGrid.superclass.initComponent.apply(this, arguments);
-		App.eventManager.on('globalsearch', this.onGlobalSearch, this);
+		App.examination.MedStandartServiceGrid.superclass.initComponent.apply(this, arguments);
 		
-		this.on('destroy', function(){
-		    App.eventManager.un('globalsearch', this.onGlobalSearch, this); 
-		},this);
 		
 		this.on('afterrender',function(){
-           	this.store.setBaseParam('staff',active_profile);
-           	this.store.load();
         })
 		
-		//this.on('rowcontextmenu', this.onContextMenu, this);
 	},
 	
-	onOpen: function(mode){
-		var rec = this.getSelected();
-		if(rec) {
-			config = {
-				ordered_service : rec.data.resource_uri,
-				patient:rec.data.patient,
-				mode:mode,
-				patient_name:rec.data.patient_name,
-				title:'Карты осмотра №'+rec.data.barcode+' ' + rec.data.patient_name
-			};
-			App.eventManager.fireEvent('launchapp', 'examcardgrid',config);
-		}
-		//}
-	},
-	
-	onAdd: function() {
-		var rec = this.getSelected();
-		if (rec && !rec.data.executed) {
-			config = {
-				closable:true,
-        		patient:rec.data.patient,
-        		patient_name: rec.data.patient_name,
-        		ordered_service:rec.data.resource_uri,
-				title: rec.data.patient_name +  ': ' + rec.data.service_name,
-				service:rec.data.service,
-				print_name:rec.data.service_name,
-				staff:this.staff
-//				scope:this
-			}
-//			App.eventManager.fireEvent('launchapp', 'examcardform',config);
-			
-			App.eventManager.fireEvent('launchapp', 'neocard',config);
-			
-        }
-		
-		
-	},
-	
-	onGlobalSearch: function(v) {
-		if(v) {
-			var letter = v.charAt(0);
-			if( letter=='#' || letter=='№') {
-				v = v.substring(1);
-			}
-			var vi = parseInt(v);
-			if (isNaN(vi)) {
-				this.storeFilter('search', v);
-			} else {
-				this.storeFilter('order__barcode', vi);
-			}
-		} else {
-			delete this.store.baseParams['search'];
-			delete this.store.baseParams['order__barcode'];
-			this.store.load();
-		}
-	},
-	
-	storeFilter: function(field, value){
-		if(!value) {
-			//console.log(this.store.baseParams[field]);
-			delete this.store.baseParams[field]
-			//this.store.setBaseParam(field, );
-		} else {
-			this.store.setBaseParam(field, value);
-		}
+	setStandart: function(standart){
+		this.store.setBaseParam('standart',standart);
+		this.choiceBtn.setDisabled(true);
 		this.store.load();
 	},
 	
-	getSelected: function() {
-		return this.getSelectionModel().getSelected()
+	btnCheckStatus: function(){
+		var sm = this.getSelectionModel();
+		records = sm.getSelections();
+		this.choiceBtn.setDisabled(!records.length)
 	},
 	
-	onOpenHistory: function(){
-		var rec = this.getSelected();
-		if (rec) {
-			config = {
-				closable:true,
-				iconCls:'silk-package',
-        		patient:rec.data.patient,
-        		patient_name: rec.data.patient_name,
-        		ordered_service:rec.data.resource_uri,
-				title: rec.data.patient_name + ': История',
-				service:rec.data.service,
-				print_name:rec.data.service_name,
-				staff:this.staff
-//				scope:this
-			}
-			App.eventManager.fireEvent('launchapp', 'patienthistory',config);
-		}
+	onServiceChoice: function(){
+		var sm = this.getSelectionModel();
+		records = sm.getSelections();
+		this.fireEvent('pushservices',records)
 	}
-
 	
 });
 
 
 
-Ext.reg('examordergrid',App.examorder.ExamOrderGrid);
+Ext.reg('medstandservgrid',App.examination.MedStandartServiceGrid);
