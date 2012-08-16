60baba9d4e97597483aaeee3253e5dcab1a0c178
Phewgin <phewgin@gmail.com>
2012-08-16 12:54:36 +0400

    Фильтрация организации в branches при построении прайса

--- apps/service/utils.py
+++ apps/service/utils.py
@@ -47,7 +47,7 @@ def revert_tree_objects(obj):
         child.move_to(obj, position='first-child')
         revert_tree_objects(child)
 
-def get_service_tree(state=None,payer=None,payment_type=None,price_type=None):
+def get_service_tree(state=None,payer=None,payment_type=None,price_type=None, date=datetime.datetime.today()):
     """
     Генерирует дерево в json-формате.
     """
@@ -78,18 +78,19 @@ def get_service_tree(state=None,payer=None,payment_type=None,price_type=None):
     args = {}
     if state:
         ignored = State.objects.filter(type='b').exclude(id=state.id)
-        args['extended_service__state']=state.id
+        args['extended_service__branches']=state
     
     if payment_type:
         args['payment_type'] = payment_type
         if payer and payment_type in [u'б',u'к']:
             args['payer'] = payer.id
 
+    args['on_date__lte'] = date
     nodes = []
     if not price_type:
         price_type = get_actual_ptype()
     values = Price.objects.filter(extended_service__is_active=True, price_type='r',type=price_type,**args).\
-        order_by('extended_service__id').\
+        order_by('extended_service__id','on_date').\
         values('extended_service__id','value','extended_service__base_service__id').\
         annotate(Max('on_date'))
     result = {}
