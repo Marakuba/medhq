1aeb256664b955e73b71bebf16032d7566be2bac
Phewgin <phewgin@gmail.com>
2012-08-16 11:12:00 +0400

    Глобальные переменные True и False

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -2,5 +2,7 @@
 
 def global_vars(request):
     return {
-        'sentry_dsn' : 'http://af0d355f077545f08c2a47e0de43fa94@localhost:9000/1'
+        'sentry_dsn' : 'http://af0d355f077545f08c2a47e0de43fa94@localhost:9000/1',
+        'True':True,
+        'False':False
     }
