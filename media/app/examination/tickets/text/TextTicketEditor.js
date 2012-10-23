120e4a55ea99f34f64b60399c5b007cc457cf725
Anatoly <4015555@gmail.com>
2012-10-23 18:25:20 +0400

    Настройка редактора текстового тикета.

--- media/app/examination/tickets/text/TextTicketEditor.js
+++ media/app/examination/tickets/text/TextTicketEditor.js
@@ -96,19 +96,21 @@ App.examination.TextTicketEditor = Ext.extend(Ext.Panel, {
 		
 		this.headerField = new Ext.form.TextField({
 			layout:'fit',
-			region:'north'
+			region:'north',
+			border:false
 		});
 		
 		this.bodyField = new Ext.ux.TinyMCE({
 			layout:'fit',
 			region:'center',
+			enableKeyEvents:true,
 			tinymceSettings: {
 				theme: "advanced",
+				skin : "o2k7",
 				plugins: "pagebreak,style,layer,table,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-				theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-				theme_advanced_buttons2: "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-				theme_advanced_buttons3: "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|",
-				theme_advanced_buttons4: "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+				theme_advanced_buttons1: "bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect,|,forecolor,backcolor",
+				theme_advanced_buttons2: "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,insertdate,inserttime,|,visualchars,nonbreaking,pagebreak",
+				theme_advanced_buttons3: "tablecontrols,|,hr,removeformat,|,sub,sup",
 				theme_advanced_toolbar_location: "top",
 				theme_advanced_toolbar_align: "left",
 				theme_advanced_statusbar_location: "bottom",
@@ -135,6 +137,12 @@ App.examination.TextTicketEditor = Ext.extend(Ext.Panel, {
 //			        	var pos = this.getPos();
 //						el.dom.setSelectionRange(pos, pos);
 			     	}, this);
+			     	
+			     	c.ed.onKeyPress.add(function(ed, e) {
+			     		if(e.charCode==10 && e.ctrlKey){
+			     			this.editComplete();
+			     		}
+					} .createDelegate(this));
 			    },
             	scope:this
             }
@@ -147,11 +155,13 @@ App.examination.TextTicketEditor = Ext.extend(Ext.Panel, {
 				this.headerField,
 				this.bodyField
 			],
+			border:false,
 			region:'center'
 		});
 		
 		this.okBtn = new Ext.Button({
-			text:'Ok',
+			iconCls:'silk-resultset-previous',
+	    	text:'Вернуться к карте',
 			handler:this.editComplete,
 			scope:this
 		})
@@ -175,7 +185,8 @@ App.examination.TextTicketEditor = Ext.extend(Ext.Panel, {
 		App.examination.TextTicketEditor.superclass.initComponent.apply(this, arguments);
 		this.on('afterrender',function(){
 			if (this.data){
-				this.loadTicket(this.data)
+				this.loadTicket(this.data);
+				this.bodyField.focus(false, 350);
 			}
 //			console.log(this.bodyField.getEl())
 //			this.glossDropDown.bindElement(this.bodyField.getEl(), this.glossDropDown);
@@ -188,6 +199,7 @@ App.examination.TextTicketEditor = Ext.extend(Ext.Panel, {
 		this.on('editorclick', function(){
 			this.glossDropDown.clearBuffer();
 		},this)
+		
 	},
 	
 	loadTicket: function(ticket){
