b7f7577ff857540727ca2be65f757c2efadddf58
Phewgin <phewgin@gmail.com>
2012-10-31 18:45:51 +0400

    связь договоров и счетов

--- media/app/accounting/contract/ContractGrid.js
+++ media/app/accounting/contract/ContractGrid.js
@@ -2,9 +2,16 @@ Ext.ns('App.accounting');
 
 
 App.accounting.ContractGrid = Ext.extend(Ext.grid.GridPanel, {
-    
+
     initComponent: function() {
-        
+
+        this.addContractBtn = new Ext.Button({
+            iconCls:'silk-add',
+            text:'Добавить договор',
+            handler:this.onAddContract.createDelegate(this),
+            scope:this
+        });
+
         this.store = new Ext.data.RESTStore({
             autoSave : false,
             autoLoad : false,
@@ -18,7 +25,8 @@ App.accounting.ContractGrid = Ext.extend(Ext.grid.GridPanel, {
             width:15
         },{
             header: "Дата",
-            dataIndex: 'date',
+            dataIndex: 'on_date',
+            renderer:Ext.util.Format.dateRenderer('d.m.Y'),
             width:20
         },{
             header: "Организация",
@@ -26,7 +34,7 @@ App.accounting.ContractGrid = Ext.extend(Ext.grid.GridPanel, {
             width:100
         },{
             header: "Филиал",
-            dataIndex: 'branch_price',
+            dataIndex: 'branch_name',
             width:100
         }];
 
@@ -38,12 +46,14 @@ App.accounting.ContractGrid = Ext.extend(Ext.grid.GridPanel, {
                 singleSelect : true,
                 listeners: {
                     rowselect: function(sm, row, rec) {
+                        this.fireEvent('rowselect',rec)
                     },
                     rowdeselect: function(sm, row, rec) {
                     },
                     scope:this
                 }
             }),
+            tbar:[this.addContractBtn],
             viewConfig : {
                 forceFit : true
                 //getRowClass : this.applyRowClass
@@ -56,7 +66,29 @@ App.accounting.ContractGrid = Ext.extend(Ext.grid.GridPanel, {
         Ext.apply(this, Ext.apply(this.initialConfig, config));
         App.accounting.ContractGrid.superclass.initComponent.apply(this, arguments);
 
+        this.on('afterrender',function(){
+            this.store.load();
+        },this)
+
     },
+
+    onAddContract: function(){
+        var contractWin = new App.accounting.ContractWindow({
+            store:this.store,
+            fn:function(record){
+                if(!record){
+                    contractWin.close();
+                    return false;
+                };
+                this.store.add(record);
+                this.store.save();
+                contractWin.close();
+            },
+            scope:this
+        });
+
+        contractWin.show();
+    }
 
 });
 
