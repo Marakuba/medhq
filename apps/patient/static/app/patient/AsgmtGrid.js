2cefa5ea608a6092950a3b566f0c6adedec8b6cd
Anatoly <4015555@gmail.com>
2013-01-21 19:26:27 +0400

    Some fixes

--- apps/patient/static/app/patient/AsgmtGrid.js
+++ apps/patient/static/app/patient/AsgmtGrid.js
@@ -80,7 +80,7 @@ App.patient.AsgmtGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 		this.setTimeButton = new Ext.Button({
 			xtype:'button',
 			text:'Назначить время',
-			hidden:this.referral_type != 'л',
+			// hidden:this.referral_type != 'л',
 			disabled:true,
 			handler:this.staffWindow.createDelegate(this, []),
 			scope:this
